3e6a511fbcf851869fd611c89ff53482549bb108
Clodagh553 <124406054@umail.ucc.ie>
2026-02-17 12:40:50 +0000

    Checking if it works

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/hey')
+def hello_world():  # put application's code here
+    return 'Heyyy!'
+
 
 if __name__ == '__main__':
     app.run()
