ca1e1eb84c816ce82793243a5447f264d9eb01b7
Clodagh553 <124406054@umail.ucc.ie>
2026-03-03 09:39:59 +0000

    Tested API - Confirmed working

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return 'This API is running! Add /search?t=harry+potter to the URL to test'
 
-@app.route('/hey')
-def hello_world2():  # put application's code here
-    return 'bye!'
+@app.route('/search')
+def search():
+    title=request.args.get('t')
+    if not title:
+        return {'error': 'No title provided'}, 400
+
+    url = f"https://openlibrary.org/search.json?title={title}"
+    response = requests.get(url)
+    return response.json(), response.status_code
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
